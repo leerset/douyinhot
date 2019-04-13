90c76f59765488dd73bdc0b18a6aa34d8f4e41e9
jianyi_dhd <jianyi.dhd@gmail.com>
2019-04-13 12:03:49 +0800

    set float x

--- app/assets/javascripts/category_formulas.js
+++ app/assets/javascripts/category_formulas.js
@@ -3,7 +3,7 @@ function calculate()
     var inputX = document.getElementById('calculate_x');
     var inputResult = document.getElementById('calculate_result');
     var category_formula_formula = document.getElementById('category_formula_formula');
-    var x = parseInt(inputX.value) || 0;
+    var x = parseFloat(inputX.value) || 0;
     var formula = category_formula_formula.value || '';
-    inputResult.value = eval(formula);
+    inputResult.value = eval(formula).toFixed(10);
 }
