2ffc27782d950a762fab3b666c757ccad4490fdd
jianyi_dhd <jianyi.dhd@gmail.com>
2019-04-14 14:40:55 +0800

    add mobile_has_idcode checking for download api

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -4,13 +4,14 @@ module Api
   module V1
     # Define actions for categorys.
     class CategoryController < ParentController
+      before_action :set_params
       before_action :save_request
 
       # GET api/v1/download.
       # Get the download url for the file and redirect to it.
       def download
         @code = 0
-        if authorized? && use_status? && need_renew? && can_use?
+        if authorized? && use_status? && need_renew? && can_use? && mobile_has_idcode?
           @category_request.update(release_status: 2)
           categories = Category.all.order(:category_number)
           render json: { success: true, code: @code, categories: CategorySerializer.build_array(categories) }
@@ -22,13 +23,18 @@ module Api
 
       private
 
+      def set_params
+        @idcode = params[:idcode]
+      end
+
       def save_request
         @category_request = CategoryRequest.create(
           app_id: @app_id,
           user_id: @user.try(:id),
           request_status: -1,
           release_status: 1,
-          request_ip: request.remote_ip
+          request_ip: request.remote_ip,
+          id_code: @idcode,
         )
       end
 
@@ -54,6 +60,13 @@ module Api
         last_request_time = @app.category_requests.where.not(id: @category_request.id).map(&:request_time).max
         return true if last_request_time < Category.last_updated_at
         @code = 4
+        false
+      end
+
+      def mobile_has_idcode?
+        return true unless @user_agent.present? && @user_agent.match(/Android|iPhone|iPad/i)
+        return true if @idcode.present?
+        @code = 7
         false
       end
 
